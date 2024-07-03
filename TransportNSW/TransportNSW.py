f5c24ea970e44f7645a0c126e81323779caf3848
Aaron Gorka <aarongorka@users.noreply.github.com>
2024-07-03 20:54:30 +1000

    Add trip planner sensor
    Add a sensor that uses the trip planner API rather than the departures
    API. This requires specifying a destination, but means you can track
    e.g. any train line that goes to your destination.

--- TransportNSW/TransportNSW.py
+++ TransportNSW/TransportNSW.py
@@ -194,3 +194,101 @@ class TransportNSW(object):
             11: "Schoolbus"
         }
         return modes.get(iconId, None)
+
+
+    def get_trip(self, origin_stop_id, destination_stop_id, excluded_means=[]):
+        """Get data about a trip from Transport NSW."""
+
+        # Default return value
+        info = {
+            ATTR_STOP_ID: 'n/a',
+            ATTR_ROUTE: 'n/a',
+            ATTR_DUE_IN: 'n/a',
+            ATTR_DELAY: 'n/a',
+            ATTR_REALTIME: 'n/a',
+            ATTR_DESTINATION: 'n/a',
+            ATTR_MODE: 'n/a'
+            }
+
+        # Build the URL including the STOP_ID and the API key
+        url = 'https://api.transport.nsw.gov.au/v1/tp/trip'
+        auth = 'apikey ' + self.api_key
+        headers = {'Accept': 'application/json', 'Authorization': auth}
+
+        if len(excluded_means) > 0:
+            params_excluded_means = {
+                "excludedMeans": "checkbox",
+                **{"exclMOT_{}".format(x): 1 for x in excluded_means}
+            }
+        else:
+            params_excluded_means = {}
+
+        params = {
+            "outputFormat": "rapidJSON",
+            "coordOutputFormat": "EPSG:4326",
+            "depArrMacro": "dep",
+            "type_origin": "any",
+            "name_origin": origin_stop_id,
+            "type_destination": "any",
+            "name_destination": destination_stop_id,
+            "calcNumberOfTrips": 1,
+            **params_excluded_means,
+        }
+
+        # Send the query and return error if something goes wrong
+        # Otherwise store the response
+        try:
+            response = requests.get(url, params=params, headers=headers, timeout=self.timeout)
+        except:
+            logger.warning("Network or Timeout error")
+            return info
+
+        # If there is no valid request (e.g. http code 200)
+        # log error and return empty object
+        if response.status_code != 200:
+            logger.warning("Error with the request sent; check api key")
+            return info
+
+        # Parse the result as a JSON object
+        result = response.json()
+
+        # If there are no journies for the query
+        # log an error and return empty object
+        try:
+            journeys = result['journeys']
+        except KeyError:
+            logger.warning("No journeys for this query")
+            return info
+
+        # Set variables
+        maxresults = 1
+        monitor = []
+        for journey in journeys:
+            origin = journey["legs"][0]["origin"]
+            time_estimated = datetime.fromisoformat(origin.get("departureTimeEstimated") or origin.get("departureTimePlanned") or origin.get("arrivalTimeEstimated") or origin.get("arrivalTimePlanned"))
+            time_planned = datetime.fromisoformat(origin.get("departureTimePlanned") or origin.get("arrivalTimePlanned"))
+            time_now = datetime.now(timezone.utc)
+
+            if journey["legs"][0].get("isRealtimeControlled") is not None:
+                info[ATTR_REALTIME] = journey["legs"][0]["isRealtimeControlled"]
+
+            info[ATTR_STOP_ID] = origin_stop_id
+            info[ATTR_ROUTE] = ",".join([leg["transportation"]["name"] for leg in journey["legs"]])
+            info[ATTR_DUE_IN] = round((time_estimated - time_now).seconds / 60)
+            info[ATTR_DELAY] = round((time_estimated - time_planned).seconds / 60)
+            info[ATTR_DESTINATION] = destination_stop_id
+            info[ATTR_MODE] = ",".join(set([self.get_mode(x["transportation"]["product"]["class"]) for x in journey["legs"]]))
+            return info
+        return info
+
+    def get_mode(self, iconId):
+        """Map the iconId to proper modes string."""
+        modes = {
+            1: "Train",
+            4: "Lightrail",
+            5: "Bus",
+            7: "Coach",
+            9: "Ferry",
+            11: "Schoolbus"
+        }
+        return modes.get(iconId, None)
